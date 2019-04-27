e75f4dee1402b989ba1b111dc563126a47713deb
Lucas Neves <lucas54neves@gmail.com>
2019-04-27 13:53:37 -0300

    Método para adicionar animal

--- src/Animais/Setor.java
+++ src/Animais/Setor.java
@@ -4,29 +4,50 @@ import Animais.*;
 import Funcionarios.*;
 
 public class Setor {
-    private final Animal mAnimais;
-    private Funcionario mResponsavel;
-    private final String mSetor;
+    private final Animal animais;
+    private Funcionario responsavel;
+    private final String setor;
+    private int quantidade_animais;
 
-    public Setor(Animal mAnimais, Funcionario mResponsavel, String mSetor) {
-        this.mAnimais = mAnimais;
-        this.mResponsavel = mResponsavel;
-        this.mSetor = mSetor;
+    public Setor(Animal animal, Funcionario responsavel, String setor) {
+        this.animais = animal;
+        this.responsavel = responsavel;
+        this.setor = setor;
     }
 
     public Animal getAnimais() {
-        return mAnimais;
+        return animais;
     }
 
     public Funcionario getResponsavel() {
-        return mResponsavel;
+        return responsavel;
     }
 
     public String getSetor() {
-        return mSetor;
+        return setor;
+    }
+
+    public int getQuantidade_animais() {
+        return quantidade_animais;
     }
 
     public void setResponsavel(Funcionario novoResponsavel) {
-        this.mResponsavel = novoResponsavel;
+        this.responsavel = novoResponsavel;
+    }
+
+    public void setQuantidade_animais(int quantidade_animais) {
+        this.quantidade_animais = quantidade_animais;
+    }
+    
+    public void adicionarAnimal(Animal animal) {
+        try {
+            if (this.getAnimais() == animal) {
+                setQuantidade_animais(getQuantidade_animais() + 1);
+            } else {
+                throw new IllegalArgumentException("Não é possível adicionar esse animal nesse setor.");
+            }
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
